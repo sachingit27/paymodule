2b92bbe6b966d29d208ebd6bb97e545679cb6de0
sachin <sachin@LAPTOP-1J9UUENO>
2021-04-17 18:07:46 -0700

    First commit

---
+++ sac/src/test/java/com/example/sac/SacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
