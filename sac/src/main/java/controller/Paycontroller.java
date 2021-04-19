fdbd0f807b0a9dc639b1ad76006982981e495278
sachin <sachin@LAPTOP-1J9UUENO>
2021-04-18 19:39:22 -0700

    get

--- sac/src/main/java/controller/Paycontroller.java
+++ sac/src/main/java/controller/Paycontroller.java
@@ -1,7 +1,21 @@
 package controller;
 
-public class controller {
-  
-  System.out.println("In controller");
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@CrossOrigin()
+public class Paycontroller {
+
+	//Show all subscriber API
+		@RequestMapping("/showallsubscriber") 
+		public void showallemployee() {
+			System.out.println("Below are list of subscriber list");
+		}
+ 
 
 }
